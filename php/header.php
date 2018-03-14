51e6cc53a3069b5455300b66fe5c5cd1573a49a1
tcinfinity <32245869+tcinfinity@users.noreply.github.com>
2018-03-14 10:57:36 +0800

    php + file

---
+++ php/header.php
@@ -0,0 +1,16 @@
+<header class="ws-theme-header" role="banner">
+  <div class="ws-theme-container">
+    <div class="ws-theme-header-inner">
+
+      <table class="WikiLogoTable WikiElement">
+        <tbody>
+          <tr>
+            <td class="WikiLogoName">
+              <a href="https://wongjinfa.github.io/">wongjinfa</a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</header>
